b599ccfce54c6b3f1bb709a02f0b0b5cc1b9681e
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-09 17:31:02 -0500

    organize icons

--- profile-view/src/data/linksData.js
+++ profile-view/src/data/linksData.js
@@ -2,18 +2,18 @@ import githubIcon from '../assets/github-mark-white.png';
 import tiktokIcon from '../assets/TikTok_Icon_Black_Circle.png';
 import whatsappIcon from '../assets/Digital_Glyph_Green.png';
 import instagramIcon from '../assets/Instagram_Glyph_White.png';
-// import loftyIcon from '../assets/';
-// import discordIcon from '../assets/';
+import threadsIcon from '../assets/threads-logo-white-01.png';
+import discordIcon from '../assets/Discord Mark Blue.svg';
 
 // username & links
 const username = 'K1ngHandy'
 const linksData = [
-    { icon: githubIcon, name: 'GitHub', url: `https://github.com/${username}` },
-    { icon: whatsappIcon, name: 'WhatsApp', url: `https://whatsapp.com/channel/0029Vb4QSVPKLaHhn8SfS33K` },
     { icon: tiktokIcon, name: 'TikTok', url: `https://www.tiktok.com/@${username}?_t=ZT-8tmP8GTOzNL&_r=1` },
+    { icon: githubIcon, name: 'GitHub', url: `https://github.com/${username}` },
+    { icon: threadsIcon, name: 'Threads', url: `https://www.threads.net/@${username}` },
+    { icon: discordIcon, name: 'Discord', url: `https://discord.com/channels/1140423533625888889/1140423534372470806` },
+    { icon: whatsappIcon, name: 'WhatsApp', url: `https://whatsapp.com/channel/0029Vb4QSVPKLaHhn8SfS33K` },
     { icon: instagramIcon, name: 'Instagram', url: `https://instagram.com/${username}` },
-    // { icon: loftyIcon, name: 'Lofty', url: "https://www.lofty.ai/refer?utm_source=growsurf&utm_medium=referrals&utm_campaign=referral-program&grsf=k-1-nghand-vurjsr" },
-    // { icon: discordIcon, name: 'Discord', url: `https://discord.com/channels/1140423533625888889/1140423534372470806` },
 ]
 
 export default linksData;
