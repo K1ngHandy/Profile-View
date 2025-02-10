a12631ac03317d470aecf29fb027ab2df7ab8247
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-10 17:44:24 -0500

    move analytics & speed insights to index

--- profile-view/src/index.js
+++ profile-view/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SpeedInsights>
+      <Analytics>
+        <App />
+      </Analytics>
+    </SpeedInsights>
   </React.StrictMode>
 );
