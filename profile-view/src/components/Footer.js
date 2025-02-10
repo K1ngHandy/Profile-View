92c3c8261a5565042f1985ca6f54815846cfa004
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-09 21:59:03 -0500

    css changes

--- profile-view/src/components/Footer.js
+++ profile-view/src/components/Footer.js
@@ -1,27 +1,11 @@
 import React from "react";
 import "../css/footer.css"
-import linksData from "../data/linksData";
-import githubIcon from "../assets/github-mark-white.png";
 
 function Footer() {
-    const link = linksData.find(link => link.name === "GitHub");
+    
 
     return (
-        <footer className="footer">
-            <p>
-                <a href={link ? link.url : '#'} 
-                    target="_blank" 
-                    rel="noopener noreferrer" 
-                    className="tooltip">
-                        K1ngHandy
-                    <span className="tooltiptext">
-                        <img src={githubIcon} alt="GitHub" className="icons" />
-                        GitHub
-                    </span>
-                </a>
-                &nbsp;&copy;2025. All rights reserved.
-            </p>
-        </footer>
+        <footer className="footer"></footer>
     )
 }
 
--- profile-view/src/components/Main.js
+++ profile-view/src/components/Main.js
@@ -3,10 +3,13 @@ import React, { useState, useEffect } from "react";
 import "../css/main.css"
 import linksData from "../data/linksData";
 import logo from "../assets/LightWatermark.png"
+import githubIcon from "../assets/github-mark-white.png";
 
 function Main() {
     const [clock, setClock] = useState(new Date());
     const [blink, setBlink] = useState(true);
+
+    const link = linksData.find(link => link.name === "GitHub");
     
     const openLink = (url) => {
         window.open(url, '_blank');
@@ -51,8 +54,9 @@ function Main() {
                     <span className="tooltiptext">{dateFormat(clock)}</span>
                 </div>
             </div>
-            <h2>Selectable Links:</h2>
+            
             <div className="grid-container">
+                <h2 className="selectable-links">Selectable Links:</h2>
                 {linksData.map((link, idx) => (
                     <button className="tooltip" key={idx} onClick={() => openLink(link.url)} title={link.name}>
                         <img src={link.icon} alt={link.name} className="icons" />
@@ -61,6 +65,19 @@ function Main() {
                 ))}
             </div>
             <hr className="divider" />
+            <p>
+                <a href={link ? link.url : '#'} 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    className="tooltip">
+                        K1ngHandy
+                    <span className="tooltiptext">
+                        <img src={githubIcon} alt="GitHub" className="icons" />
+                        GitHub
+                    </span>
+                </a>
+                &nbsp;&copy;2025. All rights reserved.
+            </p>
         </div>
     );
 }
