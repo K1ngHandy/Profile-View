e41a6f0859e80d3d310b17c9326295ebce30207f
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-10 17:44:58 -0500

    remove previous calls

--- profile-view/src/App.js
+++ profile-view/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
@@ -12,8 +10,6 @@ function App() {
       <Header />
       <Main />
       <Footer />
-      <SpeedInsights />
-      <Analytics />
     </div>
   );
 }
