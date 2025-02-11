d1ebe76782a8afc23cf9c12828ed919732a16259
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-10 19:38:16 -0500

    comment out analytics

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from "@vercel/analytics/react";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+// import { Analytics } from "@vercel/analytics/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <SpeedInsights>
-      <Analytics>
-        <App />
-      </Analytics>
-    </SpeedInsights>
+    {/* <SpeedInsights>
+      <Analytics> */}
+    <App />
+    {/* </Analytics>
+    </SpeedInsights> */}
   </React.StrictMode>
 );
