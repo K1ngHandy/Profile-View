b3fabf9792513f640f0875e9673461282c018bf4
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-10 22:12:26 -0500

    change root

--- src/index.js
+++ src/index.js
@@ -2,17 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/Index.css";
 import App from "./App";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
-// import { Analytics } from "@vercel/analytics/react";
 
 console.log("Root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <SpeedInsights>
-      <Analytics> */}
     <App />
-    {/* </Analytics>
-    </SpeedInsights> */}
   </React.StrictMode>
 );
