ef0b075409160b46846f4d8b52a0360b95272a6b
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-10 20:45:44 -0500

    add console log

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./css/App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from "./App";
 // import { SpeedInsights } from "@vercel/speed-insights/next";
 // import { Analytics } from "@vercel/analytics/react";
 
+console.log("Root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <SpeedInsights>
       <Analytics> */}
-    {console.log("Root")}
     <App />
     {/* </Analytics>
     </SpeedInsights> */}
