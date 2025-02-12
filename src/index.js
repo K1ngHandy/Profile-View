a7ba8bdb3b1582c74649c4ab31c09d3ecd8b9578
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-12 12:51:52 -0500

    move analytics

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ console.log("Root");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Analytics>
-      <App />
-    </Analytics>
+    <App />
+    <Analytics />
   </React.StrictMode>
 );
