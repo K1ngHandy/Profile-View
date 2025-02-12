5e16ff0e1a0f164c818f0e38d31ca3efa3ffb3cf
K1ngHandy <bolds-slicks0g@icloud.com>
2025-02-11 23:26:12 -0500

    update css import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles/App.css";
+import "./assets/styles/App.css";
 import Header from "./components/Header.js";
 import Main from "./components/Main.js";
 import Footer from "./components/Footer.js";
