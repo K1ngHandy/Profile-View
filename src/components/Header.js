b73d27e0825035bd8f3f5d469dab4c323e6e583d
K1ngHandy <bolds-slicks0g@icloud.com>
2025-04-08 19:04:22 -0400

    add name

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,14 @@ import '../styles/Header.css';
 import { logoLight } from '../assets/images';
 
 function Header(props) {
-	const { display_name: displayName, location } = props.data;
+	console.log('Props:', props);
+	const { display_name: displayName, company, location } = props.data;
 
 	return (
 		<header className="header">
 			<hr className="divider" />
-			<h1>{displayName}</h1>
+			<h1>{company} </h1>
+			<h2>{displayName}</h2>
 			<p>🧭 {location}</p>
 			<img
 				src={logoLight}
