793450ca701d73b7da84ec3849dcaeb59955d231
K1ngHandy <bolds-slicks0g@icloud.com>
2025-08-22 15:24:35 -0400

    Update title size

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,7 +8,7 @@ function Profile(props) {
 	return (
 		<div className="profile-container">
 			<div className="profile-content">
-				<h1>My name is Steve Handy.</h1>
+				<h2>My name is Steve Handy.</h2>
 				<p>
 					Full Stack web developer with a solid foundation in Git,
 					HTML, CSS, JavaScript, Python, React, Swift, Node.js,
